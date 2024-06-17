9d34a476e4c3b51c212f1f3fa4264b80d9be38ec
lhx@3090server <hanxiao.li@csu.edu.cn>
2024-06-17 13:07:23 +0800

    tuning drm_navigation training parameters

--- learning/config_sac.py
+++ learning/config_sac.py
@@ -44,7 +44,7 @@ class Args:
     """the discount factor gamma"""
     tau: float = 0.005
     """target smoothing coefficient"""
-    batch_size: int = 48
+    batch_size: int = 64  # 32 seems quite good
     """the batch size of sample from the reply memory"""
     learning_starts: int = 2000
     """timestep to start learning"""
@@ -74,7 +74,7 @@ class Args:
     # Roadmap env specific parameters
     k_neighbor_size: int = 30  # 15 in dep.cpp
     """number of k-nearest neighbors for edge connection"""
-    step_time: float = 3.0
+    step_time: float = 4.0
     """step time duration for executing a command from policy network"""
     coords_norm_coef_: float = 30.
     """coefficient for normalizing node coordinates"""
